ae46aecae63eb2bff92bb6b545011024f8d1cedf
Mohanbabu2004 <rmohanbabu817@gmail.com>
2026-08-10 20:40:29 +0530

    Initial commit

---
+++ src/components/pb.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react"; 
+ 
+function PB() { 
+  const [user, setUser] = useState(null); 
+ 
+  useEffect(() => { 
+    fetch("https://api.github.com/users/octocat") 
+      .then((res) => res.json()) 
+      .then((data) => setUser(data)) 
+      .catch((err) => console.log(err)); 
+  }, []); 
+ 
+  if (!user) { 
+    return <p>Loading...</p>; 
+  } 
+ 
+  return ( 
+    <div style={{ textAlign: "center", marginTop: "30px" }}> 
+      <h1>{user.login}</h1> 
+ 
+      <img 
+        src={user.avatar_url} 
+        alt="avatar" 
+        width="120" 
+      /> 
+ 
+      <p>{user.name}</p> 
+ 
+      <a href={user.html_url} target="_blank" rel="noopener noreferrer"> 
+        Visit GitHub Profile 
+      </a> 
+    </div> 
+  ); 
+}
+export default PB;
